062366d3e1f4bbdaaef1af1b472c06f40b6bd305
Ben Godfrey <bcgodfrey91@gmail.com>
2018-05-03 23:58:36 -0500

    add postcss

--- components/Hero.js
+++ components/Hero.js
@@ -4,6 +4,10 @@ function Hero() {
   return (
     <Section className='hero' styleClass="hero-tile">
       <h1>hero</h1>
+      <div className="example">helloooooooo</div>
+      <style jsx>{`
+        @import '../style.css'
+      `}</style>
     </Section>
   );
 }
