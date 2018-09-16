d7114bca3c1a50c6e30e190437dab6c5926887b2
Ben Godfrey <bcgodfrey91@gmail.com>
2018-09-16 13:59:34 -0500

    git p5 working

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,55 +1,57 @@
 <template>
   <app-section :styleClass="{ styleClass }">
-    <div ref="canvas"></div>
+    <vue-p5
+      @preload="preload"
+      @setup="setup"
+      @draw="draw"
+    >
+    </vue-p5>
   </app-section>
 </template>
 
 <script>
 import Section from './Section.vue';
+import sketch from '../helpers/sketch.js';
+import VueP5 from 'vue-p5';
+
 export default {
   name: 'Hero',
   components: {
     'app-section': Section,
+    'vue-p5': VueP5,
   },
   data () {
     return {
       styleClass: 'hero-tile',
-      script: null,
-      ps: null,
-      x: 0,
-      y: 0,
-      canvas: null,
-      colors: null,
-      colors2: null
+      img: '',
     }
   },
+  methods: {
+    preload(sketch) {
+      this.img = sketch.loadImage('https://media.licdn.com/dms/image/C4E03AQEIB7WNcY3TqQ/profile-displayphoto-shrink_200_200/0?e=1542844800&v=beta&t=3fqEWww9Vxr5jCa4bpGQjiLrBWgcmxhgJKr1mfwhoPM');
+    },
+    setup(sketch) {
+      sketch.createCanvas(this.img.height, this.img.width);
+      sketch.noStroke();
+      sketch.frameRate(60);
+    },
+    draw(sketch) {
+      const x = sketch.floor(sketch.random(this.img.height));
+      const y = sketch.floor(sketch.random(this.img.width));
+      const pix = this.img.get(x, y);
+      sketch.fill(pix, 128);
+      sketch.ellipse(x, y, 10, 10);
+    },
+  },
   mounted () {
-    this.script = p => {
-      let img;
-      p.preload = _ => {
-        img = p.loadImage('');
-      }
-
-      p.setup = _ => {
-        this.canvas = p.createCanvas(600, 420)
-        this.canvas.parent(this.$refs.canvas)
-        p.noStroke()
-        p.background(253,252,255)
-      }
-
-      p.draw = _ => {
-        this.x = p.floor(p.random(0, 600));
-        this.y = p.floor(p.random(0, 420));
-        p.fill(p.floor(p.random(0, 255)), p.floor(p.random(0, 255)), p.floor(p.random(0, 255)))
-        p.ellipse(this.x, this.y, 5, 5)
-      }
-    }
-    const P5 = require('p5')
-    this.ps = new P5(this.script)
-    console.log(this.ps)
   }
 }
 </script>
 
 <style lang="css">
+  #sketch {
+    width: 50%;
+    height: 50%;
+    border: 1px solid black;
+  }
 </style>
