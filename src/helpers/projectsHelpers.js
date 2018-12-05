d966d75cdaeec96b0e170f64f256acea19053cd8
Ben Godfrey <bengodfrey@insiderx.com>
2018-12-05 15:09:05 -0600

    small text changes

--- src/helpers/projectsHelpers.js
+++ src/helpers/projectsHelpers.js
@@ -3,42 +3,42 @@ export default [
     body: 'A static site that provides various personal project and take home challenge ideas.',
     styleclass: 'perpro',
     technologies: 'VuePress',
-    title: 'Perpro',
+    title: '💻 Perpro',
     url: 'https://github.com/bcgodfrey91/perpro'
   },
   {
     body: 'A site that fetches and provides a ranking list for shows on Crunchy Roll.',
     styleclass: 'crunchy-ratings',
     technologies: 'React, Node.js, Express, MongoDB, Cheerio',
-    title: 'Crunchy Ratings',
+    title: '💯 Crunchy Ratings',
     url: 'https://github.com/bcgodfrey91/crunchy-ratings-client'
   },
   {
-    body: 'An app that allows users to create an account and save notes-to-self.',
+    body: 'An app that allows a user to create an account and save notes to themself.',
     styleclass: 'napkin-app',
     technologies: 'React, Node.js, Serverless, AWS',
-    title: 'Napkin App',
+    title: '📝 Napkin App',
     url: 'https://github.com/bcgodfrey91/napkin-app-api'
   },
   {
     body: 'A desktop audio player application.',
     styleclass: 'amptron',
     technologies: 'Electron, Vue, Node.js, HTML5 Audio',
-    title: 'Amptron',
+    title: '🎧 Amptron',
     url: 'https://github.com/Jeff-Duke/amptron'
   },
   {
     body: 'A logic game with the objective of turning off all of the lights.',
     styleclass: 'lights-out',
     technologies: 'jQuery, Mocha, Chai, Object Oriented Programming',
-    title: 'Lights Out',
+    title: '🚨 Lights Out',
     url: 'https://github.com/bcgodfrey91/game-time-lights-out'
   },
   {
     body: 'An npm package that allows users to convert their local audio files to dataURLs for use via Electron.',
     styleclass: 'eac',
     technologies: 'Javascript, Babel',
-    title: 'electron-audio-conversion',
+    title: '🎶 electron-audio-conversion',
     url: 'https://github.com/Jeff-Duke/electron-audio-conversion'
   }
 ]
