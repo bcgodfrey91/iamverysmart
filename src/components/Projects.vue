d7f1ad4225e10dd5873926c6fda942b9732624d5
Ben Godfrey <bcgodfrey91@gmail.com>
2020-02-19 08:08:41 -0600

    update/edit

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -4,15 +4,6 @@
       <h1 class="section-title">Projects</h1>
       <div class="title-underline"></div>
     </div>
-    <div class="working-on">
-      Currently working on:
-      <a
-        v-if="dataReady"
-        v-bind:href="'https://github.com/'+currentProject.name"
-      >
-        <span>{{ currentProject.name.split("/").pop() }}</span>
-      </a>
-    </div>
     <card-list></card-list>
   </app-section>
 </template>
@@ -29,23 +20,12 @@ export default {
   data () {
     return {
       styleclass: 'projects-tile',
-      currentProject: {},
-      dataReady: false
     }
   },
   mounted () {
-    fetch('https://api.github.com/users/bcgodfrey91/events')
-      .then(response => response.json())
-      .then(data => {
-        this.currentProject = data[0].repo;
-        this.dataReady = true;
-      });
   }
 }
 </script>
 
 <style lang="css">
-  .working-on {
-    margin: 0 0 2rem;
-  }
 </style>
